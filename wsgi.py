f29be252e29adb1a207857dfadb9f947a295468f
Ornilio <orniliooficial@gmail.com>
2025-12-07 14:17:02 -0300

    ajuste no wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,13 @@
-from dotenv import load_dotenv
+import sys
 import os
+from dotenv import load_dotenv
 
-# Caminho para o diretório do projeto
-project_folder = os.path.expanduser('~/meu-portfolio') # <- IMPORTANTE: Mude 'meu-portfolio' para o nome da sua pasta no PythonAnywhere
+# Adiciona o diretório do projeto ao path do Python
+project_folder = os.path.expanduser('~/portifolio')
+if project_folder not in sys.path:
+    sys.path.insert(0, project_folder)
+
+# Carrega as variáveis de ambiente
 load_dotenv(os.path.join(project_folder, '.env'))
 
 from run import app as application
